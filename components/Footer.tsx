0a75eedac2cf91c99a3e8f4127bd4c3cc73f2864
pocketcorp <pocketcorp@protonmail.com>
2024-01-14 07:28:41 -0800

    Footer set

--- components/Footer.tsx
+++ components/Footer.tsx
@@ -3,8 +3,8 @@ import React from 'react'
 
 const Footer = () => {
   return (
-    <footer className='text-white-800'>
-        <p>Copyright 2023 JS MASTERT PRO | ALL RIGHTS RESERVED</p>
+    <footer className='text-white-800 flex-between body-text w-full gap-y-10 border-t border-black-400 bg-black-100 px-20 py-1 max-md:flex-col'>
+        <p>Copyright 2023 JS MASTERY PRO | ALL RIGHTS RESERVED</p>
 
         <div className="flex gap-x-9">
             <Link href='/terms-of-use'>Terms & Conditions</Link>
