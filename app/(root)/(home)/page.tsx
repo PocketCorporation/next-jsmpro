50bdbbff750e74694704ca8600efba83f30f8880
pocketcorp <pocketcorp@protonmail.com>
2024-01-14 07:26:27 -0800

    Footer set

--- app/(root)/(home)/page.tsx
+++ app/(root)/(home)/page.tsx
@@ -2,6 +2,12 @@ import React from 'react'
 
 export default function Page() {
   return (
-    <div>Home</div>
+    <div>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+        <h1>Home</h1>
+    </div>
   )
 }
--- components/Footer.tsx
+++ components/Footer.tsx
@@ -1,8 +1,16 @@
+import Link from 'next/link'
 import React from 'react'
 
 const Footer = () => {
   return (
-    <div>Footer</div>
+    <footer className='text-white-800'>
+        <p>Copyright 2023 JS MASTERT PRO | ALL RIGHTS RESERVED</p>
+
+        <div className="flex gap-x-9">
+            <Link href='/terms-of-use'>Terms & Conditions</Link>
+            <Link href='/privacy-policy'>Privacy Policy</Link>
+        </div>
+    </footer>
   )
 }
 
